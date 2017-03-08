6f6e9457fa014d81061d54364b199adbda34bc60
quinton <qjohnson@gatech.edu>
2017-03-08 14:37:49 -0500

    TextToSpeech added

--- app/src/main/java/comquintonj/github/star/MainActivity.java
+++ app/src/main/java/comquintonj/github/star/MainActivity.java
@@ -1,13 +1,55 @@
 package comquintonj.github.star;
 
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech textToSpeech;
+    private EditText inputText;
+    private Button inputButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate view
+        inputText = (EditText) findViewById(R.id.inputText);
+        inputButton = (Button) findViewById(R.id.inputButton);
+
+        // Instantiate TextToSpeech object
+        textToSpeech =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                textToSpeech.setLanguage(Locale.ENGLISH);
+            }
+        });
+
+        // If a user has decided to dictate
+        inputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = inputText.getText().toString();
+                sayIt(toSpeak);
+            }
+        });
     }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void sayIt(String text) {
+        String utteranceId=this.hashCode() + "";
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
 }
