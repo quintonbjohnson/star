dbf9bc42621fb23ef4693b1b2d315f35d56e2775
quinton <qjohnson@gatech.edu>
2017-03-08 14:55:42 -0500

    TextToSpeech added

--- app/src/main/java/comquintonj/github/star/MainActivity.java
+++ app/src/main/java/comquintonj/github/star/MainActivity.java
@@ -1,23 +1,30 @@
 package comquintonj.github.star;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
+
+import java.util.List;
 import java.util.Locale;
 
+/**
+ * MainActivity that displays on startup
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextToSpeech textToSpeech;
     private EditText inputText;
-    private Button inputButton;
+    private static final int SPEECH_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Instantiate view
         inputText = (EditText) findViewById(R.id.inputText);
-        inputButton = (Button) findViewById(R.id.inputButton);
+        Button inputButton = (Button) findViewById(R.id.inputButton);
 
         // Instantiate TextToSpeech object
         textToSpeech =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
@@ -46,10 +53,50 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Say the given string out loud
+     * @param text the text to be said
+     */
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     private void sayIt(String text) {
         String utteranceId=this.hashCode() + "";
         textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+    }
+
+    /**
+     * Display the Google Speech Recognizer
+     */
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        // Start the activity, the intent will be populated with the speech text
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    /**
+     * On result of the Google Speech Recognizer, use the input to transmit to the user
+     * @param requestCode the request code to decide which intent the activity came from
+     * @param resultCode the result of the intent
+     * @param data the intent that the user has come from
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 
 }
